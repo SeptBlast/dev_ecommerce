74e58a148dc1b737736281ba10aa5184fa942b0b
Devesh Kumar <deveshkumarsep12@outlook.com>
2020-10-17 23:07:27 +0530

    #0 starter commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's Create your own Amazon... 🚀</h1>
+    </div>
+
+    // Header
+
+    // Home ==> Body
+
+
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA31JwVkGTBi0QijZrogk-id7wVXmAhkaQ",
+    authDomain: "e-commerce-challange.firebaseapp.com",
+    databaseURL: "https://e-commerce-challange.firebaseio.com",
+    projectId: "e-commerce-challange",
+    storageBucket: "e-commerce-challange.appspot.com",
+    messagingSenderId: "116977695393",
+    appId: "1:116977695393:web:d625435b592041462bcee0",
+    measurementId: "G-EKWMV6BL7T"
+  };
